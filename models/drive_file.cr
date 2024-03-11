8f3863f4debf65b108e3b3d752d71ecc34ef0628
Michal Szmyd <michal.szmyd@icloud.com>
2024-03-11 17:12:09 +0100

    Updated models

--- models/drive_file.cr
+++ models/drive_file.cr
@@ -1,4 +1,10 @@
 class DriveFile
+  enum FileHosting
+    Local
+    Discord
+    AmazonS3
+  end
+
   DB.mapping({
     id: Int64,
     name: String,
@@ -9,9 +15,14 @@ class DriveFile
     archived: Bool,
     pinned: Bool,
     vibrant_color: String?,
-    folder_id: Int64,
+    folder_id: Int64?,
     user_id: Int64,
     metadata: JSON::Any,
+    hosting_source: Int32?,
+    source_signature_created_at: Time?,
+    source_signature_duration: Int32?,
+    legacy_external_storage_id: String?,
+    legacy_source_url: String?,
     created_at: Time,
     updated_at: Time
   })
--- models/folder.cr
+++ models/folder.cr
@@ -1,10 +1,22 @@
 class Folder
+      enum ExportStatus
+    Empty
+    Valid
+    Waiting
+    Processing
+    Error
+    Outdated
+  end
   DB.mapping({
     id: Int64,
     name: String,
     user_id: Int64,
     folder_private: Bool,
     parent_folder_id: Int64?,
+    export_status: Int32?,
+    exported_at: Time?,
+    export_started_at: String?,
+    export_folder_path: String?,
     created_at: Time,
     updated_at: Time
   })
