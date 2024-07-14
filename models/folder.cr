3f63c53b3adfacb9bc44b48700f55197ffcd24e8
Michal Szmyd <michal.szmyd@icloud.com>
2024-07-14 20:43:26 +0200

    Update linter

--- models/folder.cr
+++ models/folder.cr
@@ -1,5 +1,5 @@
 class Folder
-      enum ExportStatus
+  enum ExportStatus
     Empty
     Valid
     Waiting
@@ -8,17 +8,17 @@ class Folder
     Outdated
   end
   DB.mapping({
-    id: Int64,
-    name: String,
-    user_id: Int64,
-    folder_private: Bool,
-    drive_files_count: Int64,
-    parent_folder_id: Int64?,
-    export_status: Int32?,
-    exported_at: Time?,
-    export_started_at: String?,
+    id:                 Int64,
+    name:               String,
+    user_id:            Int64,
+    folder_private:     Bool,
+    drive_files_count:  Int64,
+    parent_folder_id:   Int64?,
+    export_status:      Int32?,
+    exported_at:        Time?,
+    export_started_at:  String?,
     export_folder_path: String?,
-    created_at: Time,
-    updated_at: Time
+    created_at:         Time,
+    updated_at:         Time,
   })
 end
